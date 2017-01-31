4c7ee5cc09207dcf38ebeae0cfda94a46046a63e
jignesh.rawal <jignesh.rawal@bitbucket.aon.com>
2017-01-31 16:42:24 +0530

    Node Webkit + Express.js Example
    Node Webkit + Express.js Example

---
+++ app/components/snippets/model.js
@@ -0,0 +1,51 @@
+const Datastore = require('nedb'),
+db = {},
+path = require('path');
+
+console.log(__dirname, path.join(__dirname , '../../../data/snippets.json'));
+
+db.snippets = new Datastore({ filename: path.join(__dirname , '../../../data/snippets.json') });
+
+// Load Snippets database
+db.snippets.loadDatabase(function (err) {    // Callback is optional
+	if(err) {
+		console.log("Snippets database error :", err);
+	} else {
+		console.log("Snippets database loaded successfuly.");
+	}
+});
+
+const SnippetsModel = (function(){
+
+	function getAll(callback) {
+		db.snippets.find({}, function(err, docs){
+			if(callback && typeof callback === 'function'){
+				callback({"snippets" : docs});
+			}
+		});
+	};
+
+	function addSnippet(data, callback) {
+		db.snippets.insert(data, function(err, newDoc){
+			if(err) {
+				callback({"error": err});
+			} else {
+				callback({"snippet" : newDoc});
+			}
+		});
+	};
+
+	function getSingleSnippet(id, callback) {
+		db.snippets.find({_id : id}, function(err, doc){
+			callback({"snippet" : doc})
+		});
+	};
+
+	return {
+		getAll : getAll,
+		addSnippet : addSnippet,
+		getSingleSnippet: getSingleSnippet
+	}
+})();
+
+module.exports = SnippetsModel;
